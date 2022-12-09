aee7153570cf0e4fae2e9eeef0380b807cc2b850
csymeonides <csymeonides@gmail.com>
2022-12-09 20:34:34 +0000

    chore: 2021 day 7

---
+++ src/2021/d7p2.py
@@ -0,0 +1,32 @@
+from math import inf
+
+from src.utils import run, ParsingConfig
+
+example_answer = 168
+
+example_data = """
+16,1,2,0,4,2,7,1,2,14
+"""
+
+
+parsing_config = ParsingConfig(
+    field_separator=",",
+    value_converter=int,
+)
+
+
+def _get_cost(i, pos):
+    return sum(abs(p - i) * (abs(p - i) + 1) / 2 for p in pos)
+
+
+def solve(data):
+    pos = data[0]
+    costs = [_get_cost(i, pos) for i in range(min(pos), max(pos) + 1)]
+    return min(costs)
+
+
+real_answer = None
+
+
+if __name__ == "__main__":
+    run(example_data, example_answer, parsing_config, solve, real_answer)
