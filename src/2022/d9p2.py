6e0709d8acbab34245e510ba96112a9833021d76
csymeonides <csymeonides@gmail.com>
2022-12-09 17:03:51 +0000

    chore: 2022 day 9

---
+++ src/2022/d9p2.py
@@ -0,0 +1,70 @@
+from src.utils import run, ParsingConfig, get_input_data
+
+example_answer = 36
+
+example_data = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+
+
+parsing_config = ParsingConfig(
+    value_converter=[None, int],
+)
+
+
+def _follow(tx, ty, hx, hy):
+    if tx == hx:
+        if ty < hy-1:
+            ty += 1
+        elif ty > hy+1:
+            ty -= 1
+    elif ty == hy:
+        if tx < hx-1:
+            tx += 1
+        elif tx > hx+1:
+            tx -= 1
+    elif abs(hy-ty) + abs(hx-tx) > 2:
+        tx += 1 if tx < hx else -1
+        ty += 1 if ty < hy else -1
+    return tx, ty
+
+
+def solve(data):
+    n_knots = 9
+    visited = {(0, 0)}
+    hx, hy = 0, 0
+    tail = [(0, 0) for _ in range(n_knots)]
+
+    for record in data:
+        [direction, distance] = record
+        for _ in range(distance):
+            if direction == "U":
+                hy += 1
+            elif direction == "D":
+                hy -= 1
+            elif direction == "L":
+                hx -= 1
+            else:
+                hx += 1
+
+            px, py = hx, hy
+            for i in range(n_knots):
+                tx, ty = tail[i]
+                tail[i] = _follow(tx, ty, px, py)
+                px, py = tail[i]
+            visited.add(tail[-1])
+    return len(visited)
+
+
+real_answer = 2661
+
+
+if __name__ == "__main__":
+    run(example_data, example_answer, parsing_config, solve, real_answer)
