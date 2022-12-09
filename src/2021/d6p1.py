9b8ce7405efbbe53b1abd4a2af5eb57c13cfe548
csymeonides <csymeonides@gmail.com>
2022-12-09 17:19:50 +0000

    chore: 2021 day 6

---
+++ src/2021/d6p1.py
@@ -0,0 +1,38 @@
+from src.utils import run, ParsingConfig
+
+example_answer = 5934
+
+example_data = """
+3,4,3,1,2
+"""
+
+
+parsing_config = ParsingConfig(
+    # parser_class=None,
+    # parser_func=None,
+    field_separator=",",
+    value_converter=int,
+    # single_field=True,
+    # multi_line=True,
+    # strip=False,
+)
+
+
+def solve(data):
+    state = data[0]
+    n_days = 80
+    for _ in range(n_days):
+        for i in range(len(state)):
+            if state[i] == 0:
+                state[i] = 6
+                state.append(8)
+            else:
+                state[i] -= 1
+    return len(state)
+
+
+real_answer = None
+
+
+if __name__ == "__main__":
+    run(example_data, example_answer, parsing_config, solve, real_answer)
