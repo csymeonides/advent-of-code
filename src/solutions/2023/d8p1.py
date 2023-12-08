bc11ab430ecd4cc286559f20d0b10b5ad335a08e
csymeonides <csymeonides@gmail.com>
2023-12-08 13:37:15 +0000

    chore: 2023 Day 8

---
+++ src/solutions/2023/d8p1.py
@@ -0,0 +1,58 @@
+import math
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from utils import run, ParsingConfig
+
+example_answer = 6
+
+example_data = """
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+"""
+
+
+class Parser:
+    def __init__(self):
+        self.instruction = ""
+        self.left = {}
+        self.right = {}
+
+    def parse(self, *line):
+        if not line:
+            return
+        if len(line) == 1:
+            self.instruction = line[0]
+        else:
+            self.left[line[0]] = line[2][1:4]
+            self.right[line[0]] = line[3][:3]
+
+
+parsing_config = ParsingConfig(
+    parser_class=Parser,
+)
+
+
+def solve(parser):
+    pos = "AAA"
+    steps = 0
+    while pos != "ZZZ":
+        if parser.instruction[steps % len(parser.instruction)] == "R":
+            pos = parser.right[pos]
+        else:
+            pos = parser.left[pos]
+        steps += 1
+    return steps
+
+
+if __name__ == "__main__":
+    run(
+        example_data=example_data,
+        example_answer=example_answer,
+        parsing_config=parsing_config,
+        solve=solve,
+        real_answer=20513,
+    )
